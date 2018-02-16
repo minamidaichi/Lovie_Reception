353ced4c264d38a945bb73e7dcbd727d55abb136
love.minami <love.minami@karabiner.trech>
2018-02-16 17:30:12 +0900

    demo

--- app/main.py
+++ app/main.py
@@ -7,55 +7,59 @@ from speech_api import recognize_speech_from_wav
 from text_analysis import tokenize
 import json
 
-mes_file = open("message.txt","w")
+mes_file = open("web/message.txt","w")
 mes_file.close()
 
-if __name__ == '__main__':
-        CARD = 1 #OUTPUTの指定
-        DEVICE = 0 #OUTPUTの指定
-        slack_url = 'https://hooks.slack.com/services/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'
+def write_msg(name,msg):
+    mes_file = open("web/message.txt","w")
+    mes_file.write(name+":"+msg)
+    mes_file.close()
 
-        talk_message = "カラビナテクノロジーへようこそ！どなたとのご約束ですか？"
-        mes_file = open("message.txt","w")
-        mes_file.write(talk_message)
-        mes_file.close()
+if __name__ == '__main__':
+    CARD = 1 #OUTPUTの指定
+    DEVICE = 0 #OUTPUTの指定
+    slack_url = 'https://hooks.slack.com/services/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'
+
+    ###フェーズ1
+    talk_message = "カラビナテクノロジーへようこそ！どなたとのご約束ですか？"
+    write_msg("machine",talk_message)
+    print talk_message
+    talk(talk_message, CARD, DEVICE)
+
+    ###フェーズ2
+    while True:    
+        record()
+        txt = recognize_speech_from_wav()
+        write_msg("guest",txt)
+        txt = tokenize(txt)
+        reaction_words = ["ミナミ","カワカミ"]
+        matched_list = [word for word in txt if word in reaction_words]
+        if len(matched_list) == 1:
+            to_name = matched_list[0]
+            talk_message = to_name + "ですね。所属とお名前を教えてください。"
+            write_msg("machine",talk_message)
+            print talk_message
+            talk(talk_message, CARD, DEVICE)
+            break
+        else:
+            talk_message =  "すみません、もう一度おねがいします"
+            write_msg("machine",talk_message)
+            print talk_message
+            talk(talk_message, CARD, DEVICE)
+
+    ###フェーズ3
+    while True:    
+        record()
+        txt = recognize_speech_from_wav()
+        write_msg("guest",txt)
+        from_name = txt.encode("utf-8")
+        requests.post(slack_url,data = json.dumps({'text':to_name + 'さん、' + from_name + '様がお見えになりました。'}))
+        talk_message = from_name + "さま、" + to_name + "にメッセージをおくりました。少々お待ちください。"
+        write_msg("machine",talk_message)
         print talk_message
         talk(talk_message, CARD, DEVICE)
 
-        while True:    
-            record()
-            txt = recognize_speech_from_wav()
-            txt = tokenize(txt)
-            reaction_words = ["ミナミ","カワカミ"]
-            matched_list = [word for word in txt if word in reaction_words]
-            if len(matched_list) == 1:
-                to_name = matched_list[0]
-                talk_message = to_name + "ですね。所属とお名前を教えてください。"
-                mes_file = open("message.txt","w")
-                mes_file.write(talk_message)
-                mes_file.close()
-                print talk_message
-                talk(talk_message, CARD, DEVICE)
-                break
-            else:
-                talk_message =  "すみません、もう一度おねがいします"
-                mes_file = open("message.txt","w")
-                mes_file.write(talk_message)
-                mes_file.close()
-                print talk_message
-                talk(talk_message, CARD, DEVICE)
-
-        while True:    
-            record()
-            txt = recognize_speech_from_wav()
-            from_name = txt.encode("utf-8")
-            requests.post(slack_url,data = json.dumps({'text':to_name + 'さん、' + from_name + '様がお見えになりました。'}))
-            talk_message = from_name + "さま、" + to_name + "にメッセージをおくりました。少々お待ちください。"
-            mes_file = open("message.txt","w")
-            mes_file.write(talk_message)
-            mes_file.close()
-            print talk_message
-            talk(talk_message, CARD, DEVICE)
-            mes_file = open("message.txt","w")
-            mes_file.close()
-            break
+        ###キャッシュ削除的な
+        mes_file = open("web/message.txt","w")
+        mes_file.close()
+        break
--- web/app.py
+++ web/app.py
@@ -1,13 +1,10 @@
+# coding:UTF-8
 import sys
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 import time
 from flask import Flask, request, render_template
-import os
-
-#print "--"
-#print os.listdir(".")
-#print "--"
+import json
 
 app = Flask(__name__)
 
@@ -22,10 +19,15 @@ def echo():
         past_mes = ""
         while True:
             m = open("message.txt","r")
-            time.sleep(3)
+            time.sleep(2)
             for txt in m:
                 if txt != past_mes:
-                    ws.send(unicode(txt,"utf-8"))
+                    #send_data = '{"name":"machine","message":"test"}'
+                    name = txt.split(":")[0]
+                    msg = txt.split(":")[1]
+                    send_data = {"name":name,"message":msg}
+                    print json.dumps(send_data)
+                    ws.send(json.dumps(send_data))
                     past_mes = txt
     return
 
